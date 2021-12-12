5d3b320cc702a2f05d590ff9fa3d1706713d12a7
Brittney Braxton <brittney@tablexi.com>
2021-12-11 19:57:41 -0500

    Duplicate Codepen and split into components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import MenuTab from './components/MenuTab';
+import Menu from './components/Menu';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id="our_menu" class="pt-5 pb-5">
+        <div className="container">
+          <div className="row">
+            <div class="col-lg-12">
+              <div class="page_title text-center mb-4">
+                <h1>our menu</h1>
+                <div class="single_line"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row"><MenuTab/></div>
+          <div className="row"><Menu/></div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,117 @@
+const Menu = () => {
+  return (
+    <div class="tab-content col-lg-12" id="myTabContent">
+      <div
+        class="tab-pane fade show active"
+        id="breakfast"
+        role="tabpanel"
+        aria-labelledby="breakfast-tab"
+      >
+        <div class="row">
+          <div class="col-md-6">
+            <div class="single_menu">
+              <img src="https://i.imgur.com/kbpceNv.jpg" alt="burger" />
+              <div class="menu_content">
+                <h4>
+                  Chicken Burger <span>$24</span>
+                </h4>
+                <p>
+                  Aperiam tempore sit,perferendis numquam repudiandae porro
+                  voluptate dicta saepe facilis.
+                </p>
+              </div>
+            </div>
+            <div class="single_menu">
+              <img src="https://i.imgur.com/lYKUORL.jpg" alt="black coffee" />
+              <div class="menu_content">
+                <h4>
+                  Black coffee <span>$20</span>
+                </h4>
+                <p>
+                  Aperiam tempore sit,perferendis numquam repudiandae porro
+                  voluptate dicta saepe facilis.
+                </p>
+              </div>
+            </div>
+            <div class="single_menu">
+              <img src="https://i.imgur.com/AXAHrf6.jpg" alt="fried rice" />
+              <div class="menu_content">
+                <h4>
+                  Fried Rice <span>$45</span>
+                </h4>
+                <p>
+                  Aperiam tempore sit,perferendis numquam repudiandae porro
+                  voluptate dicta saepe facilis.
+                </p>
+              </div>
+            </div>
+            <div class="single_menu">
+              <img src="https://i.imgur.com/YkDi8Nb.jpg" alt="meat" />
+              <div class="menu_content">
+                <h4>
+                  meat <span>$24</span>
+                </h4>
+                <p>
+                  Aperiam tempore sit,perferendis numquam repudiandae porro
+                  voluptate dicta saepe facilis.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="single_menu">
+              <img src="https://i.imgur.com/TAq7lDR.jpg" alt="burger" />
+              <div class="menu_content">
+                <h4>
+                  Chicken Burger <span>$24</span>
+                </h4>
+                <p>
+                  Aperiam tempore sit,perferendis numquam repudiandae porro
+                  voluptate dicta saepe facilis.
+                </p>
+              </div>
+            </div>
+            <div class="single_menu">
+              <img src="https://i.imgur.com/lYKUORL.jpg" alt="black coffee" />
+              <div class="menu_content">
+                <h4>
+                  Black coffee <span>$20</span>
+                </h4>
+                <p>
+                  Aperiam tempore sit,perferendis numquam repudiandae porro
+                  voluptate dicta saepe facilis.
+                </p>
+              </div>
+            </div>
+            <div class="single_menu">
+              <img src="https://i.imgur.com/AXAHrf6.jpg" alt="fried rice" />
+              <div class="menu_content">
+                <h4>
+                  Fried Rice <span>$45</span>
+                </h4>
+                <p>
+                  Aperiam tempore sit,perferendis numquam repudiandae porro
+                  voluptate dicta saepe facilis.
+                </p>
+              </div>
+            </div>
+            <div class="single_menu">
+              <img src="https://i.imgur.com/YkDi8Nb.jpg" alt="meat" />
+              <div class="menu_content">
+                <h4>
+                  meat <span>$24</span>
+                </h4>
+                <p>
+                  Aperiam tempore sit,perferendis numquam repudiandae porro
+                  voluptate dicta saepe facilis.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
